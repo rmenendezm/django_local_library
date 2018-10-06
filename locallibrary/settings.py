f0c6764aff71aca805b0f81a657060d1357f4723
Raul Menendez <rmenendezm@gmail.com>
2018-10-06 19:49:52 -0400

    Added files and changes required for deployment to heroku

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -129,8 +129,8 @@ LOGIN_REDIRECT_URL = '/'
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587 
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
-EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
+EMAIL_HOST_USER = os.environ.get('SENDGRID_USERNAME', 'app110923721@heroku.com')
+EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_PASSWORD', 'jkrdtnzp6564')
 
 
 # Heroku: Update database configuration from $DATABASE_URL.
